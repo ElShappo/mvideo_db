9d7fe79949d542b3796e41129931d353d22731ec
Deni <deni.shaipp@gmail.com>
2022-11-14 15:08:38 +0300

    added sql with mvideo

--- server.js
+++ server.js
@@ -37,123 +37,121 @@ console.log(items_within_stores);
 (async () => {
       const createdUsers = await User.bulkCreate(users);
 
-      for (let i=0; i<address_books.length; ++i) {
-        address_books[i] = Object.assign({user_id: createdUsers.at(i).id}, address_books[i]);
-      }
+      // for (let i=0; i<address_books.length; ++i) {
+      //   address_books[i] = Object.assign({user_id: createdUsers.at(i).id}, address_books[i]);
+      // }
 
-      await Address_book.bulkCreate(address_books); 
-      const createdManufacturers = await Manufacturer.bulkCreate(manufacturers);
+      // await Address_book.bulkCreate(address_books); 
+      // const createdManufacturers = await Manufacturer.bulkCreate(manufacturers);
 
-      for (let i=0; i<items.length; ++i) {
-        let availableIds = retrieveArray(createdManufacturers);
-        let randomId = faker.helpers.arrayElement(availableIds);
-        items[i] = Object.assign(items[i], {manufacturer_id: faker.helpers.arrayElement(availableIds)});
-      }
+      // for (let i=0; i<items.length; ++i) {
+      //   let availableIds = retrieveArray(createdManufacturers);
+      //   let randomId = faker.helpers.arrayElement(availableIds);
+      //   items[i] = Object.assign(items[i], {manufacturer_id: faker.helpers.arrayElement(availableIds)});
+      // }
 
-      const createdItems = await Item.bulkCreate(items);
+      // const createdItems = await Item.bulkCreate(items);
 
-      for (let i=0; i<carts.length; ++i) {
-        let availableUserIds = retrieveArray(createdUsers);
-        let availableItemIds = retrieveArray(createdItems);
+      // for (let i=0; i<carts.length; ++i) {
+      //   let availableUserIds = retrieveArray(createdUsers);
+      //   let availableItemIds = retrieveArray(createdItems);
 
-        let randomUserId = 0;
-        let randomItemId = 0;
+      //   let randomUserId = 0;
+      //   let randomItemId = 0;
 
-        while (true) {
-        // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
-          let toBreak = true;
+      //   while (true) {
+      //   // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
+      //     let toBreak = true;
 
-          randomUserId = faker.helpers.arrayElement(availableUserIds);
-          randomItemId = faker.helpers.arrayElement(availableItemIds);
+      //     randomUserId = faker.helpers.arrayElement(availableUserIds);
+      //     randomItemId = faker.helpers.arrayElement(availableItemIds);
 
-          let mergedRandom = randomUserId.toString() + randomItemId.toString();
+      //     let mergedRandom = randomUserId.toString() + randomItemId.toString();
 
-          for (let j=0; j<i; ++j) {
-            let mergedPresent = carts[j].user_id.toString() + carts[j].item_id.toString();
+      //     for (let j=0; j<i; ++j) {
+      //       let mergedPresent = carts[j].user_id.toString() + carts[j].item_id.toString();
 
-            if (mergedPresent === mergedRandom) {
-              toBreak = false;
-              break;
-            }
-          }
-          if (toBreak) {
-            break;
-          }
-        }
-        carts[i] = Object.assign({user_id: randomUserId, item_id: randomItemId}, carts[i]);
-      }
+      //       if (mergedPresent === mergedRandom) {
+      //         toBreak = false;
+      //         break;
+      //       }
+      //     }
+      //     if (toBreak) {
+      //       break;
+      //     }
+      //   }
+      //   carts[i] = Object.assign({user_id: randomUserId, item_id: randomItemId}, carts[i]);
+      // }
 
-      await Cart.bulkCreate(carts);
+      // await Cart.bulkCreate(carts);
 
-      for (let i=0; i<favourite_items.length; ++i) {
-        let availableUserIds = retrieveArray(createdUsers);
-        let availableItemIds = retrieveArray(createdItems);
+      // for (let i=0; i<favourite_items.length; ++i) {
+      //   let availableUserIds = retrieveArray(createdUsers);
+      //   let availableItemIds = retrieveArray(createdItems);
 
-        let randomUserId = 0;
-        let randomItemId = 0;
+      //   let randomUserId = 0;
+      //   let randomItemId = 0;
 
-        while (true) {
-        // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
-          let toBreak = true;
+      //   while (true) {
+      //   // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
+      //     let toBreak = true;
 
-          randomUserId = faker.helpers.arrayElement(availableUserIds);
-          randomItemId = faker.helpers.arrayElement(availableItemIds);
+      //     randomUserId = faker.helpers.arrayElement(availableUserIds);
+      //     randomItemId = faker.helpers.arrayElement(availableItemIds);
 
-          let mergedRandom = randomUserId.toString() + randomItemId.toString();
+      //     let mergedRandom = randomUserId.toString() + randomItemId.toString();
 
-          for (let j=0; j<i; ++j) {
-            let mergedPresent = favourite_items[j].user_id.toString() + favourite_items[j].item_id.toString();
+      //     for (let j=0; j<i; ++j) {
+      //       let mergedPresent = favourite_items[j].user_id.toString() + favourite_items[j].item_id.toString();
 
-            if (mergedPresent === mergedRandom) {
-              toBreak = false;
-              break;
-            }
-          }
-          if (toBreak) {
-            break;
-          }
-        }
-        favourite_items[i] = {user_id: randomUserId, item_id: randomItemId};
-      }
+      //       if (mergedPresent === mergedRandom) {
+      //         toBreak = false;
+      //         break;
+      //       }
+      //     }
+      //     if (toBreak) {
+      //       break;
+      //     }
+      //   }
+      //   favourite_items[i] = {user_id: randomUserId, item_id: randomItemId};
+      // }
 
-      await Favourite_item.bulkCreate(favourite_items);
+      // await Favourite_item.bulkCreate(favourite_items);
 
-      const createdStores = await Store.bulkCreate(stores);
+      // const createdStores = await Store.bulkCreate(stores);
 
-      for (let i=0; i<favourite_stores.length; ++i) {
-        let availableUserIds = retrieveArray(createdUsers);
-        let availableStoreIds = retrieveArray(createdStores);
+      // for (let i=0; i<favourite_stores.length; ++i) {
+      //   let availableUserIds = retrieveArray(createdUsers);
+      //   let availableStoreIds = retrieveArray(createdStores);
 
-        let randomUserId = 0;
-        let randomStoreId = 0;
+      //   let randomUserId = 0;
+      //   let randomStoreId = 0;
 
-        while (true) {
-        // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
-          let toBreak = true;
+      //   while (true) {
+      //   // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
+      //     let toBreak = true;
 
-          randomUserId = faker.helpers.arrayElement(availableUserIds);
-          randomStoreId = faker.helpers.arrayElement(availableStoreIds);
+      //     randomUserId = faker.helpers.arrayElement(availableUserIds);
+      //     randomStoreId = faker.helpers.arrayElement(availableStoreIds);
 
-          let mergedRandom = randomUserId.toString() + randomStoreId.toString();
+      //     let mergedRandom = randomUserId.toString() + randomStoreId.toString();
 
-          for (let j=0; j<i; ++j) {
-            let mergedPresent = favourite_stores[j].user_id.toString() + favourite_stores[j].store_id.toString();
+      //     for (let j=0; j<i; ++j) {
+      //       let mergedPresent = favourite_stores[j].user_id.toString() + favourite_stores[j].store_id.toString();
 
-            if (mergedPresent === mergedRandom) {
-              toBreak = false;
-              break;
-            }
-          }
-          if (toBreak) {
-            break;
-          }
-        }
-        favourite_stores[i] = {user_id: randomUserId, store_id: randomStoreId};
-      }
+      //       if (mergedPresent === mergedRandom) {
+      //         toBreak = false;
+      //         break;
+      //       }
+      //     }
+      //     if (toBreak) {
+      //       break;
+      //     }
+      //   }
+      //   favourite_stores[i] = {user_id: randomUserId, store_id: randomStoreId};
+      // }
 
-      await Favourite_store.bulkCreate(favourite_stores);
-
-      console.log("Orders section!");
+      // await Favourite_store.bulkCreate(favourite_stores);
 
       for (let i=0; i<orders.length; ++i) {
         let availableIds = retrieveArray(createdUsers);
@@ -163,38 +161,38 @@ console.log(items_within_stores);
 
       const createdOrders = await Order.bulkCreate(orders); 
 
-      for (let i=0; i<items_within_stores.length; ++i) {
-        let availableItemIds = retrieveArray(createdItems);
-        let availableStoreIds = retrieveArray(createdStores);
+      // for (let i=0; i<items_within_stores.length; ++i) {
+      //   let availableItemIds = retrieveArray(createdItems);
+      //   let availableStoreIds = retrieveArray(createdStores);
 
-        let randomItemId = 0;
-        let randomStoreId = 0;
+      //   let randomItemId = 0;
+      //   let randomStoreId = 0;
 
-        while (true) {
-        // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
-          let toBreak = true;
+      //   while (true) {
+      //   // this loop ensures that generated userId and itemId make up unique sequence (i.e. PK)
+      //     let toBreak = true;
 
-          randomItemId = faker.helpers.arrayElement(availableItemIds);
-          randomStoreId = faker.helpers.arrayElement(availableStoreIds);
+      //     randomItemId = faker.helpers.arrayElement(availableItemIds);
+      //     randomStoreId = faker.helpers.arrayElement(availableStoreIds);
 
-          let mergedRandom = randomItemId.toString() + randomStoreId.toString();
+      //     let mergedRandom = randomItemId.toString() + randomStoreId.toString();
 
-          for (let j=0; j<i; ++j) {
-            let mergedPresent = items_within_stores[j].item_id.toString() + items_within_stores[j].store_id.toString();
+      //     for (let j=0; j<i; ++j) {
+      //       let mergedPresent = items_within_stores[j].item_id.toString() + items_within_stores[j].store_id.toString();
 
-            if (mergedPresent === mergedRandom) {
-              toBreak = false;
-              break;
-            }
-          }
-          if (toBreak) {
-            break;
-          }
-        }
-        items_within_stores[i] = {store_id: randomStoreId, item_id: randomItemId};
-      }
+      //       if (mergedPresent === mergedRandom) {
+      //         toBreak = false;
+      //         break;
+      //       }
+      //     }
+      //     if (toBreak) {
+      //       break;
+      //     }
+      //   }
+      //   items_within_stores[i] = {store_id: randomStoreId, item_id: randomItemId};
+      // }
 
-      await Items_within_store.bulkCreate(items_within_stores);
+      // await Items_within_store.bulkCreate(items_within_stores);
 
 
   
